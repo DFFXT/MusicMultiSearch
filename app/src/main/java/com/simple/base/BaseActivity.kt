30d9f2574a534a5a8a8d67758ef80cc535d44910
qinfei <qinfei@pwrd.com>
2019-08-06 10:22:19 +0800

    windowInset

--- app/src/main/java/com/simple/base/BaseActivity.kt
+++ app/src/main/java/com/simple/base/BaseActivity.kt
@@ -1,13 +1,20 @@
 package com.simple.base
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), OnApplyWindowInsetsListener {
 
+    var topInset = 0
+    var bottomInset = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView, this)
         if (layoutId() != 0) {
             setContentView(layoutId())
         }
@@ -20,5 +27,12 @@ abstract class BaseActivity : AppCompatActivity() {
 
     open fun beforeView() {
 
+    }
+
+    override fun onApplyWindowInsets(v: View, insets: WindowInsetsCompat): WindowInsetsCompat {
+        topInset = insets.systemWindowInsetTop
+        bottomInset = insets.systemWindowInsetBottom
+        ViewCompat.onApplyWindowInsets(v, insets)
+        return insets
     }
 }
